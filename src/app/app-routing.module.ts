432a64fd7ee6f3620646b2e717b9902b9f5a7c65
yogendra singh <yogendra@i2k2.com>
2021-07-11 13:22:16 +0530

    hand gesture demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'home'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { filter, map, shareReplay, withLatestFrom } from 'rxjs/operators';
+import { HandGesture } from './hand-gesture.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-tensorflow-hand-gestures-demo';
+  @ViewChild('video') video: ElementRef<HTMLVideoElement>;
+  @ViewChild('canvas') canvas: ElementRef<HTMLCanvasElement>;
+  @ViewChild('home') homeLink: ElementRef<HTMLAnchorElement>;
+  @ViewChild('about') aboutLink: ElementRef<HTMLAnchorElement>;
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+    opened$ = this._recognizer.swipe$.pipe(
+      filter((value) => value === 'left' || value === 'right'),
+      map((value) => value === 'right')
+    );
+  
+    selection$ = this._recognizer.gesture$.pipe(
+      filter((value) => value === 'one' || value === 'two'),
+      map((value) => (value === 'one' ? 'home' : 'about'))
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver,private _recognizer: HandGesture, private _router: Router) {
+    this._recognizer.gesture$
+    .pipe(
+      filter((value) => value === 'ok'),
+      withLatestFrom(this.selection$)
+    )
+    .subscribe(([_, page]) => this._router.navigateByUrl(page));
+  }
+
+  get stream(): MediaStream {
+    return this._recognizer.stream;
+  }
+
+  ngAfterViewInit(): void {
+    this._recognizer.initialize(
+      this.canvas.nativeElement,
+      this.video.nativeElement
+    );
+  }
+  
 }
